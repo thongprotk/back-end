78c35a4997b938fc4696fbb676391001df4cdfae
linhdx <linhdx@appota.com>
2024-10-02 17:21:28 +0700

    fix bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 const app = express();
 const cors = require("cors");
 // const server = createServer();
-const io = new Server(3000, {
+const io = new Server(5000, {
   cors: {
     origin: "*",
   },
@@ -24,57 +24,87 @@ io.on("connection", (socket) => {
   io.to(_id).emit("connection", "connected");
   console.log(`user connected: ${_id}`);
 
-  socket.on("createRoom", (roomID) => {
-    room[roomID] = { firstPlayerChoice: null, firstPlayerResult: null };
+  // socket.on("createRoom", (roomID) => {
+  //   room[roomID] = { firstPlayerChoice: null, firstPlayerResult: null };
 
-    socket.join(room[roomID]);
+  //   socket.join(room[roomID]);
 
-    console.log(`room ${roomID} created by ${_id}`);
+  //   console.log(`room ${roomID} created by ${_id}`);
 
-    socket.to(roomID).emit("playersConnected", { roomID: roomID });
-  });
+  //   socket.to(roomID).emit("playersConnected", { roomID: roomID });
+  // });
+
   socket.on("joinRoom", (roomID) => {
-    // const roomSize = io.sockets.adapter.rooms.get(roomID).size;
-    // if (roomSize >= 2) {
-    //   return socket.emit("roomFull");
-    // }
-    // if (io.sockets.adapter.r√ooms.has(roomID)) {
     socket.join(roomID);
-    room[roomID] = { secondPlayerChoice: null };
 
-    socket.to(roomID).emit("playersConnected", { roomID });
-    console.log(`room id: ${roomID}`);
-    return socket.emit("playersConnected", { roomID });
-    // }
+    if (!room[roomID]) {
+      room[roomID] = { player1: socket.id, player2: null };
+
+      // Gửi sự kiện cho player1 biết họ là player1
+      socket.emit("playersConnected", { roomID, player1: true });
+    } else if (!room[roomID].player2) {
+      // Nếu phòng đã có player1, gán người chơi này là player2
+      room[roomID].player2 = socket.id;
+
+      // Gửi sự kiện cho player2 biết họ là player2
+      socket.emit("playersConnected", { roomID, player1: false });
+
+      // Gửi thông báo cho cả player1 rằng player2 đã tham gia
+      socket
+        .to(room[roomID].player1)
+        .emit("playersConnected", { roomID, player1: true });
+    }
+    // console.log("room", room);
+    // socket.to(roomID).emit("playersConnected", { roomID, player1: !player1 });
+
+    // return socket.emit("playersConnected", { roomID, player1: !player1 });
+    // // }
   });
 
   socket.on("p1Choice", (data) => {
+    console.log("p1 choice");
     if (data) {
       const choice = data.rpsChoice;
       const roomID = data.roomID;
+      if (!room[roomID]) {
+        room[roomID] = {
+          firstPlayerChoice: choice, // hoặc các thuộc tính khác nếu cần
+        };
+      } else {
+        room[roomID].firstPlayerChoice = choice;
+      }
+      // socket.to(roomID).emit("p1Choice", {
+      //   rpsValue: choice,
+      //   score: room[roomID].firstPlayerResult,
+      // });
+      console.log(room);
 
-      room[roomID].firstPlayerChoice = choice;
-      socket.to(roomID).emit("p1Choice", {
-        rpsValue: choice,
-        score: room[roomID].firstPlayerResult,
-      });
-      if (room[roomID].secondPlayerChoice) {
-        return determineWinnerChoice(roomID);
+      if (
+        !!room[roomID].firstPlayerChoice &&
+        !!room[roomID].secondPlayerChoice
+      ) {
+        determineWinnerChoice(roomID);
       }
     }
   });
+
   socket.on("p2Choice", (data) => {
+    console.log("p2choice");
     if (data) {
       const choice = data.rpsChoice;
       const roomID = data.roomID;
 
       room[roomID].secondPlayerChoice = choice;
-      socket.to(roomID).emit("p2Choice", {
-        rpsValue: choice,
-        score: room[roomID].secondPlayerResult,
-      });
-      if (room[roomID].firstPlayerChoice) {
-        return determineWinnerChoice(roomID);
+      // socket.to(roomID).emit("p2Choice", {
+      //   rpsValue: choice,
+      //   score: room[roomID].secondPlayerResult,
+      // });
+      console.log("room", room);
+      if (
+        !!room[roomID].firstPlayerChoice &&
+        !!room[roomID].secondPlayerChoice
+      ) {
+        determineWinnerChoice(roomID);
       }
     }
   });
@@ -84,6 +114,7 @@ io.on("connection", (socket) => {
     room[roomID].firstPlayerChoice = null;
     return socket.to(roomID).emit("playAgain");
   });
+
   socket.on("exitGame", (data) => {
     const roomID = data.roomID;
     if (data.player) {
@@ -109,24 +140,36 @@ const determineWinnerChoice = (roomID) => {
     winner = "draw";
   } else if (room[roomID].firstPlayerChoice == FIGHT_OPTION.BUA) {
     if (room[roomID].secondPlayerChoice == FIGHT_OPTION.BAO) {
-      winner = " p2";
+      winner = "2";
     } else {
-      winner = "p1";
+      winner = "1";
     }
   } else if (room[roomID].firstPlayerChoice == FIGHT_OPTION.KEO) {
     if (room[roomID].secondPlayerChoice == FIGHT_OPTION.BUA) {
-      winner = "p2";
+      winner = "2";
     } else {
-      winner = "p1";
+      winner = "1";
     }
   } else if (room[roomID].firstPlayerChoice == FIGHT_OPTION.BAO) {
     if (room[roomID].secondPlayerChoice == FIGHT_OPTION.KEO) {
-      winner = "p2";
+      winner = "2";
     } else {
-      winner = "p1";
+      winner = "1";
     }
   }
-  return io.sockets.to(roomID).emit("winner", winner);
+  console.log("roomID", roomID);
+  if (roomID) {
+    io.sockets.emit("winner", {
+      roomID,
+      winner,
+    });
+    console.log(
+      "Emitted winner to room:",
+      room[roomID],
+      "with winner:",
+      winner
+    );
+  }
 };
 
 // server.listen(3000, async () => {
